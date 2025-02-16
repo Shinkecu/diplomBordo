4a9a318a9bb10e43448e1afc26fc97398ff2c515
vooleedoom <itbass@mail.ru>
2025-02-17 02:05:28 +0300

    mobile_adapting_masters_reviews

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,29 +15,45 @@
                     class="location-button">МЕСТОПОЛОЖЕНИЕ</button></a>
         </div>
     </div>
-
     <div class="advantages">
         <h1 id="advantages">ПРЕИМУЩЕСТВА</h1>
-        <div class="advantages_container">
-            <div class="advantages_card">
-                <p>Сторудники с большим опытом и результатами</p>
-            </div>
-            <div class="advantages_card">
-                <p>Комфортная и уютная атмосфера</p>
-            </div>
-            <div class="advantages_card">
-                <p>Вернем деньги если не устроит результат</p>
-            </div>
-            <div class="advantages_card">
-                <p>Совмещение отдыха и преображения</p>
-            </div>
-            <div class="advantages_card">
-                <p>Качественные и премиальные материалы для работы</p>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-2 mb-4">
+                    <div class="advantages_card">
+                        <p>Сотрудники с большим опытом и результатами</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-2 mb-4">
+                    <div class="advantages_card">
+                        <p>Комфортная и уютная атмосфера</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-2 mb-4">
+                    <div class="advantages_card">
+                        <p>Вернем деньги, если не устроит результат</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-2 mb-4">
+                    <div class="advantages_card">
+                        <p>Совмещение отдыха и преображения</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-2 mb-4">
+                    <div class="advantages_card">
+                        <p>Качественные и премиальные материалы для работы</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-2 mb-4">
+                    <div class="advantages_card">
+                        <p>Индивидуальный подход к каждому клиенту</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="services">
+    <div class="services container">
         <h1 id="services">УСЛУГИ</h1>
         @auth
             <a class="adminButton" href="{{ route('categories.index') }}"><button class="btn btn-danger">Изменить список категорий</button></a>
@@ -63,7 +79,7 @@
     </div>
 
     <h1 class="slider-container-name">РАБОТЫ НАШИХ МАСТЕРОВ</h1>
-    <div class="slider-container">
+    <div class="slider-container container">
         <h1 class="slider-names">Маникюра:</h1>
         <div class="slider">
             <img src="images/image-1.png" alt="Работа 1">
@@ -138,44 +154,63 @@
 
 
     <h1 id="reviews" class="review-container-name">ОТЗЫВЫ КЛИЕНТОВ</h1>
+
     @auth
-        <a class="adminButton" href="{{route('reviews.index')}}"><button class="btn btn-danger">Изменить отзывы</button></a>
+        <div class="text-center mb-5">
+            <a class="btn btn-danger" href="{{ route('reviews.index') }}">Изменить отзывы</a>
+        </div>
     @endauth
 
-    <div class="review-container">
-        @foreach($reviews as $review)
-            <div class="review">
-                <img src="{{ asset($review->image) }}" alt="Отзыв {{ $review->id }}">
-                <h3>{{ $review->name }}</h3>
-                <p>{{ $review->content }}</p>
-                <div class="date">{{ $review->created_at->format('d m y')  }}</div>
+    <div class="container">
+        <div class="reviews-scroll-container">
+            <div class="reviews-scroll">
+                @foreach($reviews as $review)
+                    <div class="review-card">
+                        <div class="card h-100 shadow review">
+                            <div class="p-2"> <!-- Добавлен отступ вокруг изображения -->
+                                <img src="{{ asset($review->image) }}" class="card-img-top" alt="Отзыв {{ $review->id }}">
+                            </div>
+                            <div class="card-body text-center">
+                                <h3 class="card-title">{{ $review->name }}</h3>
+                                <p class="card-text">{{ $review->content }}</p>
+                                <div class="date">{{ $review->created_at->format('d m y') }}</div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
+        </div>
     </div>
 
+    <h1 id="masters" class="text-center mt-5 mb-4">МАСТЕРА</h1>
 
-    <h1 id="masters" class="masters-container-name">МАСТЕРА</h1>
     @auth
-    <a class="adminButton" href="{{ route('edit.masters') }}">
-        <button class="btn btn-danger">Изменить список мастеров</button>
-    </a>
-
+        <div class="text-center mb-5">
+            <a href="{{ route('edit.masters') }}" class="btn btn-danger">Изменить список мастеров</a>
+        </div>
     @endauth
-    <div class="masters-container">
-        @foreach ($masters as $master)
-            <div class="masters-card">
-                <img src="{{ asset($master->image) }}" alt="{{ $master->name }}">
-                <div class="masters-content">
-                    <h3>{{ $master->name }}</h3>
-                    <p>{{ $master->position }}</p>
-                    <a href="{{ route('master.info', ['master_id' => $master->id]) }}"><button
-                            class="masters-btn">Подробнее</button></a>
-                </div>
+
+    <div class="container">
+        <div class="masters-scroll-container">
+            <div class="masters-scroll">
+                @foreach ($masters as $master)
+                    <div class="master-card">
+                        <div class="card h-100 shadow">
+                            <img src="{{ asset($master->image) }}" class="card-img-top" alt="{{ $master->name }}" style="height: 300px; object-fit: cover;">
+                            <div class="card-body text-center">
+                                <h3 class="card-title">{{ $master->name }}</h3>
+                                <p class="card-text text-muted">{{ $master->position }}</p>
+                                <a href="{{ route('master.info', ['master_id' => $master->id]) }}" class="btn btn-success">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
+        </div>
     </div>
+
     <h1 class="question-container-name">ВОПРОС-ОТВЕТ</h1>
-    <div class="question-container">
+    <div class="question-container container">
         <div class="question-card">
             <div class="question">
                 Возможно ли утреннеое обслуживание вне режима работы?
