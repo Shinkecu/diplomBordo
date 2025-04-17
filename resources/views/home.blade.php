0568d92e8189a68edf34e597d90cda27ae81957d
vooleedoom <itbass@mail.ru>
2025-04-18 02:19:05 +0300

    commit5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -262,4 +262,276 @@
             });
         });
     </script>
+    <style>@font-face {
+        font-family: 'kaiseiopti-bold';
+        src: url('/fonts/KaiseiOpti-Bold.ttf');
+    }
+    @font-face {
+        font-family: 'kaiseiopti-medium';
+        src: url('/fonts/KaiseiOpti-Medium.ttf');
+    }
+    @font-face {
+        font-family: 'kaiseiopti-regular';
+        src: url('/fonts/KaiseiOpti-Regular.ttf');
+    }
+    @font-face {
+        font-family: 'aboreto-regular';
+        src: url('/fonts/Aboreto-Regular.ttf');
+    }
+    @font-face {
+        font-family: 'playfair';
+        src: url('/fonts/Playfair.ttf');
+    }
+    @font-face {
+        font-family: 'playfair-italic';
+        src: url('/fonts/Playfair-Italic.ttf');
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: 'playfair';
+    }
+
+    .navbar {
+        background-image: url('/images/header_bg.png');
+        background-size: cover;
+        background-position: center;
+        height: 20em;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
+
+    .header-container {
+        width: 85%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        position: relative;
+    }
+
+    .nav-links {
+        list-style: none;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+    }
+
+    .nav-links li {
+        text-align: center;
+    }
+
+    .nav-links li a {
+        text-decoration: none;
+        color: white;
+        font-family: 'kaiseiopti-regular';
+        font-size: 24px;
+        display: block;
+        transition: all 0.3s ease; /* Добавляем плавный переход */
+    }
+
+    /* Анимация при наведении */
+    .nav-links li a:hover {
+        color: #ffcc00; /* Меняем цвет текста */
+        transform: scale(1.1); /* Увеличиваем текст */
+    }
+
+    .left-links,
+    .right-links {
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        border-bottom: 1px solid white;
+        padding-bottom: 20px;
+    }
+
+    .left-links {
+        margin-right: 20px;
+    }
+
+    .right-links {
+        margin-left: 20px;
+    }
+
+    .logo {
+        text-align: center;
+        padding-bottom: 20px;
+    }
+
+    .logo-main {
+        font-family: 'aboreto-regular';
+        font-size: 96px;
+        color: white;
+        line-height: 1;
+        margin-bottom: 10px;
+    }
+
+    .logo-sub .text {
+        font-family: 'aboreto-regular';
+        font-size: 40px;
+        color: white;
+        line-height: 1;
+    }
+
+    .logo-sub {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
+        position: relative;
+        margin-top: 10px;
+    }
+
+    /* Стили для бургер-меню */
+    .burger-menu {
+        display: none;
+        cursor: pointer;
+        position: absolute;
+        right: 20px;
+        top: 20px;
+    }
+
+    .burger-bar {
+        width: 30px;
+        height: 5px;
+        background-color: white;
+        margin: 5px 0;
+    }
+
+    /* Мобильное меню */
+    .mobile-nav {
+        display: none;
+        flex-direction: column;
+        align-items: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.9);
+        z-index: 1000;
+        overflow-y: auto;
+        padding-top: 60px;
+        transition: transform 0.3s ease-in-out;
+        transform: translateX(-100%);
+    }
+
+    .menu-toggle:checked + .burger-menu + .mobile-nav {
+        display: flex;
+        transform: translateX(0);
+    }
+
+    .mobile-nav li {
+        margin: 20px 0;
+    }
+
+    .mobile-nav li a {
+        font-size: 24px;
+        color: white;
+    }
+
+    #first-left, #first-right {
+        padding-left: 5%;
+    }
+
+    #last-left, #last-right {
+        padding-right: 5%;
+    }
+
+    /* -------------------------------------------Footer------------------------------------------------- */
+
+    footer {
+        background-color: #2C2C2C;
+        padding-top: 50px;
+        padding-bottom: 50px;
+    }
+
+    .footer-links {
+        display: flex;
+        padding: 0px;
+        gap: 5%;
+        list-style: none;
+    }
+
+    .footer-links-container {
+        width: 30%;
+        margin-left: 5%;
+    }
+
+    .footer-links a {
+        text-decoration: none;
+        color: white;
+        transition: all 0.3s ease; /* Добавляем плавный переход */
+    }
+
+    .footer-links a:hover {
+        color: #ffcc00; /* Меняем цвет текста при наведении */
+    }
+
+    .footer-rights-info {
+        color: white;
+        margin-left: 5%;
+        font-family: 'playfair';
+        font-weight: 100;
+        font-size: 20px;
+        margin-top: 50px;
+    }
+
+    .footer-offer-info {
+        width: 30%;
+        color: white;
+        margin-left: 5%;
+        font-family: 'playfair';
+        font-weight: 100;
+        font-size: 16px;
+        margin-top: 50px;
+        margin-bottom: 50px;
+    }
+
+    .legal-information {
+        color: white;
+        text-decoration: none;
+        margin-left: 5%;
+    }
+
+    /* Медиа-запросы для мобильных устройств */
+    @media screen and (max-width: 768px) {
+        .navbar {
+            height: auto;
+        }
+
+        .header-container {
+            flex-direction: column;
+            align-items: flex-start;
+        }
+
+        .left-links, .right-links {
+            display: none;
+        }
+
+        .logo {
+            padding-bottom: 10px;
+        }
+
+        .logo-main {
+            font-size: 72px;
+        }
+
+        .logo-sub .text {
+            font-size: 32px;
+        }
+
+        /* Показываем бургер-меню */
+        .burger-menu {
+            display: block;
+        }
+    }
+    </style>
 @endsection
